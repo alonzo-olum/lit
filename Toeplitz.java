f97e9e05f7370ee5d8536dcbeb6d2ae7df2a89b9
alonzo-olum <alondiff@gmail.com>
2026-03-15 18:24:51 +0300

    dumb me: simple solution

---
+++ Toeplitz.java
@@ -0,0 +1,23 @@
+import static java.util.AbstractMap.SimpleEntry;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
+import java.util.HashMap;
+
+public class Toeplitz {
+	public static void main(String[] args) {
+		int[][] cells = {{5, 9, 4, 1}, {2, 5, 9, 4}, {3, 2, 5, 9}};
+		System.out.println(Toeplitz.toeplitz(cells));
+	}
+
+	public static boolean toeplitz(int[][] cells) {
+		for (int r = 1; r < cells.length; r++) {
+			for (int c = 1; c < cells[0].length; c++) {
+				if (cells[r][c] != cells[r - 1][c - 1])
+					return false;
+			}
+		}
+		return true;
+	}
+}
